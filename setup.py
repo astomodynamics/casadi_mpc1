bbc80bf947d0e2aa69f42280df7244465b0ddbc9
Tomo <tomohiro.sasaki@gatech.edu>
2024-09-12 18:11:56 -0400

    fix bug (#1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['casadi_mpc'],
+    packages=['casadi_mpc1'],
     package_dir={'': 'src'},
 )
 
